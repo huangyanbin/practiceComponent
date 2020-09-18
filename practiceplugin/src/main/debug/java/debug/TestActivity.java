129965a45af4d6ab9f72759177e290cab8b6052c
huangyanbin <873825232@qq.com>
2020-09-18 17:09:43 +0800

    <M>ARouter

--- practiceplugin/src/main/debug/java/debug/TestActivity.java
+++ practiceplugin/src/main/debug/java/debug/TestActivity.java
@@ -1,12 +1,29 @@
 package debug;
 
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import {{fullName}}.R;
+import com.app.jianguyu.jiangxidangjian.cc.testplugin.R;
+import com.jxrs.component.base.BaseActivity;
 
 /**
  * 组件默认测试入口类
  */
-public class TestActivity extends AppCompatActivity {
+public class TestActivity extends BaseActivity {
 
 
+    @Override
+    public int setLayoutID() {
+        return R.layout.activity_test;
+    }
+
+    @Override
+    public void initView(@Nullable Bundle savedInstanceState) {
+        TextView tv = findViewById(R.id.tvTitle);
+        tv.setText("欢迎通过远程下载加载{{pluginName}}组件");
+    }
 }
